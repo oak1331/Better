98f8c7e4bcfa6d66ea97825c064ec712bd58f98c
oak1331 <oaks1331@gmail.com>
2021-05-28 08:36:25 +0900

    indexアクションの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:user).order('created_at DESC')
+    @posts = Post.limit(6).order('created_at DESC')
   end
 
   def new
