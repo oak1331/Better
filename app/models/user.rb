ddf30deee9b6e8bb56d6c88755ce8b9bcd9d1b21
oak1331 <oaks1331@gmail.com>
2021-06-07 05:50:55 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :user
 
   validates :nickname, presence: true
 
