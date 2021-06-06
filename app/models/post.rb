02dfba7e3ff31bf10b023d0f30ced5340ed39094
oak1331 <oaks1331@gmail.com>
2021-06-06 09:33:29 +0900

    アソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :time_slot
   belongs_to :category
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 
   validates :nickname, presence: true
 end
