118f2896cdac793473d6b2bc31f4b00c30ea3334
oak1331 <oaks1331@gmail.com>
2021-06-06 09:36:34 +0900

    メソッドの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, source: :post
 
   validates :nickname, presence: true
+
+  def liked_by?(post_id)
+    likes.where(post_id: post_id).exists?
+  end
 end
