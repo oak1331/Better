637fb2c375cf3ce6cbc6f7ae0c3fa59504e8dc15
oak1331 <oaks1331@gmail.com>
2021-06-14 16:30:43 +0900

    投稿時のエラーメッセージ実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create(post_params)
-    if post.save
+    @post = Post.create(post_params)
+    if @post.save
       redirect_to root_path
     else
       render :new
